50e5d0cd0f5c4309854ccefb1e5831229a1854b4
minas-mbp <whiplashoo721@gmail.com>
2026-04-08 10:19:51 +0300

    Initial Commit

---
+++ Blanket/BlanketApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlanketApp.swift
+//  Blanket
+//
+//  Created by Minas Giannekas on 8/4/26.
+//
+
+import SwiftUI
+
+@main
+struct BlanketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blanket/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Blanket
+//
+//  Created by Minas Giannekas on 8/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
